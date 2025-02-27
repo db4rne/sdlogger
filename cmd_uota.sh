d807f32ad1cb832870bf780c565fc46de81cab07
Arne Zachlod <arne@nerdkeller.org>
2025-02-27 11:43:03 +0100

    uota and upload test script

---
+++ cmd_uota.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo '__sdlogger__ {"cmd": "UOTA", "wlan_ssid": "sorglosinternet.de", "wlan_password": "foobar23"}' > /dev/kmsg
---
+++ cmd_upload.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+echo '__sdlogger__ {"cmd": "upload", "wlan_ssid": "sorglosinternet.de", "wlan_password": "foobar23", "router_mac": "1c:61:b4:4f:b8:ea", "upload_server": "http://192.168.133.197/upload/"}' > /dev/kmsg
