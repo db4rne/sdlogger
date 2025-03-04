76693b97d717084ae34eca6e5518d18ed709422f
Arne Zachlod <arne@nerdkeller.org>
2025-03-04 16:54:41 +0100

    requests library got changed, so put it into the update

--- install_program_on_esp32.sh
+++ install_program_on_esp32.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 mip install sdcard
-python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 mip install requests
 python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 mip install github:mkomon/uota
 
 python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 fs cp main.py :main.py
+python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 fs cp requests.py :requests.py
 python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 fs cp uota.cfg :uota.cfg
 python ~/hacks/micropython/tools/mpremote/mpremote.py connect /dev/ttyACM0 fs cp version :version
 
