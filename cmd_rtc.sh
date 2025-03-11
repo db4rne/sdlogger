dd370d23e99a53aaf69e3ef07f2d2eb155101849
Arne Zachlod <arne@nerdkeller.org>
2025-03-11 15:39:11 +0100

    cmd_rtc.sh: set the actual time from the host system

--- cmd_rtc.sh
+++ cmd_rtc.sh
@@ -1,9 +1,5 @@
 #! /bin/sh
 
-# year = 2025
-# month = 02
-# day = 18
-# hour = 14
-# min = 59
-# sec = 6
-echo '__sdlogger__ {"cmd": "RTC", "epoch": 1739890746}' > /dev/kmsg
+EPOCH=$(date +%s)
+
+echo "__sdlogger__ {\"cmd\": \"RTC\", \"epoch\": ${EPOCH}}" > /dev/kmsg
