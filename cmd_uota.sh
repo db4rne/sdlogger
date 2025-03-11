320ecdbbc58a3e10d121c8b6ed2999a897fea3e5
Arne Zachlod <arne@nerdkeller.org>
2025-03-11 15:40:29 +0100

    cmd_uota.sh: use settings from OpenWRT host

--- cmd_uota.sh
+++ cmd_uota.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-echo '__sdlogger__ {"cmd": "UOTA", "wlan_ssid": "sorglosinternet.de", "wlan_password": "foobar23"}' > /dev/kmsg
+WLANSSID="$(uci get wireless.default_radio0.ssid)"
+WLANENC="$(uci get wireless.default_radio0.encryption)"
+
+COMMAND="__sdlogger__ {\"cmd\": \"UOTA\", \"wlan_ssid\": \"${WLANSSID}\", "
+
+if [ "$WLANENC" != "none" ]; then
+	WLANPASS="$(uci get wireless.default_radio0.key)"
+	COMMAND="${COMMAND}\"wlan_password\": \"${WLANPASS}\""
+fi
+
+COMMAND="${COMMAND}}"
+echo "${COMMAND}" > /dev/kmsg
+
